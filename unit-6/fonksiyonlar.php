70f3047d56a610c6dbdbb0c4718531d780f513bb
Uğurcan <gnys.ugurcan49@gmail.com>
2023-07-14 14:13:33 +0300

    Day 3

--- unit-6/fonksiyonlar.php
+++ unit-6/fonksiyonlar.php
@@ -1,10 +1,32 @@
 <?php
-
+    /*
     function deneme()
     {
-        print "HELLO";
+        return  "HELLO";
     }
-    deneme();
     
+    $a = deneme();
+    echo $a;//print le aynı işlevi görür
 
+    
+    function sum($num1 , $num2)
+    {
+        return ($num1 + $num2);
+    }
+    $summ = sum(3,4);
+    echo $summ;
+    */
+    $name = 'ugurcan';
+
+    function namesurname($surname)
+    {
+        global $name;
+        return $name . ' ' . $surname;
+        //return $GLOBALS['name'] . ' ' . $surname;
+    }
+   // echo namesurname('guneysu');
+
+$write = "hello world";
+echo substr($write,0,4);//substr kısaltma için kullanılır
+//strlen() toplam karakter sayısını bulur
 ?>
---
+++ unit-8/dosya_cagirma.php
@@ -0,0 +1,11 @@
+<?php
+//include 
+//require
+//ile dosya çağrılma yapılır
+
+include 'cagrilan_dosya.php'; 
+require 'cagrilan_dosya.php';
+//include_once ile tek sefer çağrılmak istenen dosyalar kullanılır
+echo 'deneme';
+
+?>
