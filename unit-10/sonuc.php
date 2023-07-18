c8870c491b4dba52665717dc1a0fe6a3a773b59c
Uğurcan <gnys.ugurcan49@gmail.com>
2023-07-18 09:11:32 +0300

    Day 5

--- unit-10/sonuc.php
+++ unit-10/sonuc.php
@@ -1,11 +1,11 @@
 <?php
 
+error_reporting(E_ALL);
+
 if ($_FILES['dosya']['error'] == 4) {
     echo 'Lütfen Dosya Seçiniz.';
-}
-else {
-    //print_r($_FILES['dosya']);
-    if(is_uploaded_file($_FILES['dosya']['tmp_name'])) {
+} else {
+    if (is_uploaded_file($_FILES['dosya']['tmp_name'])) {
         $gecerli_dosya_uzantilari = [
             'image/jpeg',
             'image/png',
@@ -15,30 +15,25 @@ else {
         $gecerli_dosya_boyutu = (1024 * 1024 * 3);
 
         $dosya_uzantisi = $_FILES['dosya']['type'];
-    }
-    else {
-        echo 'Dosya Yüklenirken Bir Sorun Oluştu.';
 
-        if(in_array($dosya_uzantisi, $gecerli_dosya_uzantilari )) {
-            if($gecerli_dosya_boyutu >= $_FILES['dosya']['size']) {
-                $yukle = move_uploaded_file($_FILES['dosya']['tmp_name'], 'upload/' . $_FILES['dosya']['name']);
-                if($yukle) {
+        if (in_array($dosya_uzantisi, $gecerli_dosya_uzantilari)) {
+            if ($gecerli_dosya_boyutu >= $_FILES['dosya']['size']) {
+                $yukle = move_uploaded_file($_FILES['dosya']['tmp_name'], '/var/www/html/upload/' . $_FILES['dosya']['name']);
+                if ($yukle) {
                     echo '<h3>Dosya Yükleme Başarılı</h3>';
-                    echo '<img src="upload/' .$_FILES['dosya']['name'] . '">';
+                    echo '<img src="upload/' . $_FILES['dosya']['name'] . '">';
+                } else {
+                    echo 'Yüklenirken Bir Sorun Oluştu.';
                 }
-                else {
-                    echo 'Yüklenirken Bir Sorun Oluştu.'
-                }
-            }
-            else {
+            } else {
                 echo 'Yüklenecek dosya en fazla 3MB olabilir';
             }
+        } else {
+            echo 'Dosya Türü Sadece jpeg, png veya gif olabilir.';
         }
-        else {
-            echo 'Dosya Türü Sadece jpeg,png veya gif olabilir.';
-        }
+    } else {
+        echo 'Dosya Yüklenirken Bir Sorun Oluştu.';
     }
 }
-//dosyanın bütün içeriğini görmek için kullanılır
 
 ?>
